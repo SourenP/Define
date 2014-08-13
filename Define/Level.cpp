31dfb57deb979f339a92a15be4b17139d432097e
SourenP <souren@papazian.info>
2014-08-13 15:02:25 +0400

    Working on drawing the hex map properly

--- Define/Level.cpp
+++ Define/Level.cpp
@@ -2,23 +2,24 @@
 
 bool Level::Start(int windowSize)
 {
+	m_tileArray = new Tile[MAP_SIDE_LENGTH];
 	double tileRadius = (windowSize / MAP_DIAMETER)/2.0;
 	double tileDiameter = 2 * tileRadius;
 	double tileHeight = sqrt(3) * tileRadius;
-	int k = 0;
+	int tileIndex = 0;
 
 	for (int i = 0; i < MAP_SIDE_LENGTH; ++i)
 	{
-		double mapOffset = (MAP_DIAMETER - MAP_SIDE_LENGTH - i )/2.0;
-		double tileOffset = mapOffset * tileHeight + tileHeight / 2.0;
-		m_tileArray[k].Create(tileRadius, sf::Vector2f(tileOffset, i * (tileRadius + tileRadius/2) + tileRadius));
+		double tileOffsetAmt = (MAP_DIAMETER - MAP_SIDE_LENGTH - i )/2.0;	// amount of tiles row is offset by 
+		double tileOffset = tileOffsetAmt * tileHeight + tileHeight / 2.0;  // tileOffsetAmt + tiles already place on row (in pixels)
+		m_tileArray[tileIndex] = new Tile(tileRadius, sf::Vector2f(tileOffset, i * (tileRadius + tileRadius / 2) + tileRadius/2));
 
-		k++;
-		for (int j = 1; j < MAP_SIDE_LENGTH + i; ++j)
+		tileIndex++;
+		for (int j = 1; j < MAP_SIDE_LENGTH + i; ++j) 
 		{
 			tileOffset += tileHeight;
-			m_tileArray[k].Create(tileRadius, sf::Vector2f(tileOffset, i * (tileRadius + tileRadius / 2) + tileRadius));
-			k++;
+			m_tileArray[tileIndex] = new Tile(tileRadius, sf::Vector2f(tileOffset, i * (tileRadius + tileRadius / 2) + tileRadius / 2));
+			tileIndex++;
 		}
 	}
 
