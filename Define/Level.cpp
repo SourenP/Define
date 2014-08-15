790757268242827e0410305a170ff5392b73cb28
SourenP <souren@papazian.info>
2014-08-16 02:27:54 +0400

    Best hexagon (flat topped)
    Hexagon grid is now drawn properly.
    What is left:
    1.  Give the Tiles their coordinate numbers (preferably converted to
    cube coord)
    2. Set the map offset of the hexagon grid to align grid symmetrically.
    3. Check for memory leaks (especially ~Level)

--- Define/Level.cpp
+++ Define/Level.cpp
@@ -3,27 +3,34 @@
 #include <iostream>
 using namespace std;
 
+
 Level::Level(int windowSize)
 {
-	tileTable = new unordered_map<string, Tile*>();
-	float tileHeight = 20;
+	tileTable = new	unordered_map<string, Tile*>();
+	float tileHeight = windowSize / MAP_DIAMETER;
 	float tileRadius = tileHeight / sqrt(3);
 	float tileDiameter = 2 * tileRadius;
-	float xOffset = tileRadius;
+	float xMapOffset = 0;
+	float yMapOffset = 0;
+	float xOffset = 0;
+	float yOffset;
 	int rowMin = 0;
 	int rowMax = MAP_SIDE_LENGTH - 1;
 
 	for (int c = 1-MAP_SIDE_LENGTH; c < MAP_SIDE_LENGTH; ++c)
 	{
+		xOffset += tileDiameter * 3.0 / 4.0;
+		yOffset = tileHeight*((MAP_DIAMETER - abs(rowMax - rowMin)) / 2.0);	
 		for (int r = rowMin; r <= rowMax; ++r)
 		{
-			(*tileTable)[c + "_" + r] = new Tile(tileRadius, sf::Vector2f(xOffset, (c + MAP_SIDE_LENGTH-1)*tileHeight)); // wrong x & y
+			(*tileTable)[to_string(c) + "_" + to_string(r)] = new Tile(tileRadius, sf::Vector2f(xOffset + xMapOffset, yOffset + yMapOffset));
+			yOffset += tileHeight;
 		}
 		if (c < 0)
 			rowMin--;
 		else
 			rowMax--;
-		xOffset += tileDiameter;
+		cout << endl;
 	}
 }
 
@@ -32,14 +39,13 @@ void Level::Draw(sf::RenderWindow& rw)
 	for (auto it = tileTable->begin(); it != tileTable->end(); ++it)
 	{
 		it->second->Draw(rw);
-		cout << "$" << endl;
 	}
 }
 
 Level::~Level()
 {
-	for (auto it = tileTable->begin(); it != tileTable->end(); ++it)
-	{	
+	for (auto it = tileTable->begin(); it != tileTable->end();)
+	{
 		it = tileTable->erase(it);
 	}
 	delete tileTable;
--- Define/Level.h
+++ Define/Level.h
@@ -25,7 +25,7 @@ private:
 	Level(const Level&);
 	Level& operator=(const Level&);
 
-	static const int MAP_SIDE_LENGTH = 4;
+	static const int MAP_SIDE_LENGTH = 6;
 	static const int MAP_DIAMETER = (2 * MAP_SIDE_LENGTH) - 1;
 	static const int TILE_COUNT = MAP_SIDE_LENGTH * (3 * MAP_SIDE_LENGTH - 1) - MAP_DIAMETER;
 	unordered_map<string, Tile*>* tileTable;
